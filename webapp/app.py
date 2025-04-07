79f3cf97fb6bb7cf525c3a4f1b49789c9c01ae6b
Nikolai Pak <nikolai_pak@epam.com>
2025-04-07 20:39:43 +0200

    added adding images and getting the metadata

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,55 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 app = Flask(__name__)
+s3 = boto3.client('s3')
+BUCKET_NAME = 'web-site-pak-nikolai'
+
+def get_metadata(image_name):
+    try:
+        response = s3.head_object(Bucket=BUCKET_NAME, Key=image_name)
+        return response['Metadata']
+    except s3.exceptions.NoSuchKey:
+        return None
+
+@app.route('/download/<image_name>', methods=['GET'])
+def download_image(image_name):
+    try:
+        response = s3.get_object(Bucket=BUCKET_NAME, Key=image_name)
+        return response['Body'].read(), 200
+    except s3.exceptions.NoSuchKey:
+        return "Image not found", 404
+
+@app.route('/metadata/<image_name>', methods=['GET'])
+def show_metadata(image_name):
+    metadata = get_metadata(image_name)
+    if metadata:
+        return jsonify(metadata), 200
+    else:
+        return "Image not found", 404
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+    
+    try:
+        s3.upload_fileobj(file, BUCKET_NAME, file.filename)
+        return "File uploaded successfully", 200
+    except NoCredentialsError:
+        return "AWS credentials not found", 403
+
+@app.route('/delete/<image_name>', methods=['DELETE'])
+def delete_image(image_name):
+    try:
+        s3.delete_object(Bucket=BUCKET_NAME, Key=image_name)
+        return "Image deleted successfully", 200
+    except s3.exceptions.NoSuchKey:
+        return "Image not found", 404
 
 def get_region():
     try:
