272bbe74448ff85c795495addcba72c20140570b
Nikolai Pak <nikolai_pak@epam.com>
2025-04-13 19:58:27 +0200

    fixed metdata and added writing to rds

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,7 @@ import boto3
 from botocore.exceptions import NoCredentialsError
 import pymysql
 import argparse
+import os
 
 app = Flask(__name__)
 s3 = boto3.client('s3')
@@ -14,7 +15,12 @@ db_info = {}
 def get_metadata(image_name):
     try:
         response = s3.head_object(Bucket=BUCKET_NAME, Key=image_name)
-        return response#['Metadata']
+        metadata = {}
+        metadata["last_modified"] = response['LastModified']
+        metadata["file_size"] = response['ContentLength']
+        metadata["file_extension"] = os.path.splitext(image_name)[1]
+        metadata["file_name"] = image_name
+        return metadata
     except s3.exceptions.NoSuchKey:
         return f"{image_name} not found in bucket {BUCKET_NAME}" #None
 
@@ -44,7 +50,7 @@ def upload_image():
     
     try:
         s3.upload_fileobj(file, BUCKET_NAME, file.filename)
-        add_metadata_to_db(file.filename)
+        add_metadata_to_db(get_metadata(file.filename))
         return "File uploaded successfully", 200
     except NoCredentialsError:
         return "AWS credentials not found", 403
@@ -74,7 +80,7 @@ def health_check():
     """Endpoint for health checks."""
     return jsonify({"status": get_region()}), 200
 
-def add_metadata_to_db(filename):
+def add_metadata_to_db(metadata):
     db_host = db_info["db_host"]
     db_user = db_info["db_user"]
     db_password = db_info["db_password"]
@@ -92,13 +98,22 @@ def add_metadata_to_db(filename):
         with connection.cursor() as cursor:
             # Create table (if needed)
             cursor.execute("""
+                DROP TABLE  images
+            """)
+            cursor.execute("""
                 CREATE TABLE IF NOT EXISTS images (
                     id INT AUTO_INCREMENT PRIMARY KEY,
-                    filename VARCHAR(255),
+                    file_name VARCHAR(255),
+                    file_extension VARCHAR(255),
+                    file_size VARCHAR(255),
+                    last_modified VARCHAR(255)
                 )
             """)
             # Insert data
-            cursor.execute("INSERT INTO images (filename) VALUES (%s)", (filename,))
+            cursor.execute("INSERT INTO images (file_name, file_extension, file_size, last_modified) VALUES (%s, %s, %s, %s)",
+                           (metadata["file_name"], metadata["file_extension"], metadata["file_size"], metadata["last_modified"]))
+            cursor.execute(f'SELECT * FROM {db_info["db_name"]}')
+            print(cursor.fetchall())
             connection.commit()
 
 def get_arguments():
@@ -130,9 +145,17 @@ def get_arguments():
 
 if __name__ == "__main__":
     args = get_arguments()
+
     db_info["db_host"] = args.db_host
     db_info["db_user"] = args.db_user
     db_info["db_password"] = args.db_password
     db_info["db_name"] = args.db_name
 
+    conn = pymysql.connect(host=db_info["db_host"],
+                       user=db_info["db_user"],
+                       password=db_info["db_password"])
+
+    conn.cursor().execute(f'CREATE DATABASE IF NOT EXISTS {db_info["db_name"]}')
+
     app.run(host="0.0.0.0", port=8080)
+    # python3 app.py  --db_user admin --db_password password --db_name images --db_host database.c3yeosk6ivcl.eu-north-1.rds.amazonaws.com
